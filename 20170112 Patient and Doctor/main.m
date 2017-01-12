508ca9903f3f8306499f47fc9d7344de45b4294d
mlinglhl <minhung.ling@gmail.com>
2017-01-12 11:55:25 -0500

    Initial Commit

---
+++ 20170112 Patient and Doctor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  20170112 Patient and Doctor
+//
+//  Created by Minhung Ling on 2017-01-12.
+//  Copyright © 2017 Minhung Ling. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
